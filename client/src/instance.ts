955b36573334787dfc50094a86a0f92a47635c8d
Amr-Azab <20194736@fue.edu.eg>
2024-03-21 16:59:17 +0200

    update url

--- client/src/instance.ts
+++ client/src/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/donation",
+  baseURL: "https://donationserver.onrender.com/api/donation",
   withCredentials: true,
 });
 
